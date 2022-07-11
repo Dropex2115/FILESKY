f17cda915d6c84130d3f03c866b71a42f73ae89f
Oliwier Mazurek <o.mazurek@technikumczestochowa.spsk.pl>
2022-07-11 14:21:13 +0200

    Dodaj pliki projektów.

---
+++ FILESKY/My Project/AssemblyInfo.vb
@@ -0,0 +1,34 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Informacje ogólne o zestawie zależą od poniższego 
+' zestawu atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+' związane z zestawem.
+
+' Przejrzyj wartości atrybutów zestawu
+<Assembly: AssemblyTitle("FILESKY")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("FILESKY")>
+<Assembly: AssemblyCopyright("Copyright ©  2022")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Poniższy identyfikator GUID odpowiada atrybutowi ID biblioteki typów, jeśli ten projekt jest uwidaczniany w modelu COM
+<Assembly: Guid("998702c3-ebc8-4414-b26e-2f858fabd9c6")>
+
+' Informacje o wersji zestawu obejmują następujące cztery wartości:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' Możesz określić wszystkie te wartości lub użyć wartości domyślnych numerów kompilacji i poprawki, 
+' stosując znak „*”, jak pokazano poniżej:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
